94d46d4f878060afdf08652c884ee388d0acb98a
Brianna Gordon <brigordon574@gmail.com>
2021-07-05 10:35:16 -0700

    commit tree 18da524dccd3b177c8ad884bb64789d89f007c32

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,25 @@
+class Artist 
+  attr_reader :name
+  @@all=[]
+    def initialize (name)
+       @name = name
+        @@all << self
+    end
+     def self.all
+        @@all 
+     end
+     def songs 
+        song.all.select do |song|
+            self ==song.artist 
+        end
+    end
+     def new_song (name,genre)
+        song.new(name,self,genre)
+     end
 
+     def genres 
+        self.songs.map do |song|
+            song.genre
+        end
+     end
+end
--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,15 @@
+def Genre
+  attr_accessor :name
+  @@all = []
+  def  initialize (name)
+    @name = name
+    
+ end
+  def songs 
+    @songs >> self
+  end 
 
+  def Artist
+    @songs.map{|genre|genre.artist}
+  end
+end
